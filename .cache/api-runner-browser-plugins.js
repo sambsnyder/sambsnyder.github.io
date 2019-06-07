83d71847d7e03057211451328ca09135f9a7afdd
samuel <samuelbsnyder@gmail.com>
2019-06-07 11:55:53 -0700

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/samuel/School/GitHub/sambsnyder.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/samuel/School/GitHub/sambsnyder.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
